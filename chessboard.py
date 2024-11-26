4ac625bc8eb36a987aa5e6d95e0d3520b16c7a6f
Amsel11 <annelene.schulze@gmail.com>
2024-11-26 13:09:18 +0100

    added coloured grids

--- chessboard.py
+++ chessboard.py
@@ -5,8 +5,8 @@ import random
 import os
 
 def get_steps(size):
-    k = int(np.log2(size))  # gets the power of 2 for the size (e.g., 64 -> 6)
-    return [2**i for i in range(k)]  # creates [1,2,4,8,16,32] for size=64
+    k = int(np.log2(size))  
+    return [2**i for i in range(k)]  
 
 def chessboardmaker(size, steps): 
     board = np.zeros(size*size)
@@ -14,54 +14,56 @@ def chessboardmaker(size, steps):
     for k in range(len(steps)):
         for j in range(0,len(board),1):
             row = j // size
-            #print (row, j) 
-            #print (j)
             board[steps[k]*j:steps[k]*(j+2)] = ((-1)**j)*(-1)**row
         board_flipped = -1*board
         grids.append((board.reshape(size,size)))
         grids.append(board_flipped.reshape(size,size))
-        board = np.zeros(size**2)
-        #grids.append(-1*board.reshape(size,size))     # inverted
-        
+        board = np.zeros(size**2)        
     return grids
 
-import random
+boards = chessboardmaker(64,get_steps(64))
 
-def coloured_grids(grid, n):
+def coloured_grids1(grid):
+    # Make sure to only change the elements which were "white"
     mask = (grid == -1)
-    colours = np.random.rand(n, 3)
-    result = np.zeros((n, *grid.shape, 3))
-    result[:, mask] = colours[:, np.newaxis, :]
+    colour = np.random.rand(3)  
+    result = np.zeros(grid.shape + (3,))  # More explicit way to add RGB dimension
     
-    return result[0]
+    for i in range(3):  
+        layer = result[:,:,i]  
+        layer[mask] = colour[i]  
+    
+    return result
 
-size = 4
-boards = chessboardmaker(size,get_steps(size))
-print (boards[1])
+#We could ask for a directory
+choice = input("Do you want to make a new folder[Y/n]: ").lower()
+if choice.startswith('y'): 
+    directory = input('give me the name of the directory: ').lower()
+    save_dir = os.path.join(os.getcwd(), directory)
+else:
+    save_dir = os.path.join(os.getcwd(), "pattern_images")
 
+os.makedirs(save_dir, exist_ok=True)
+
+print ('You have made', len(boards),'until now.')
+n = int(input("How many colours do you want to create for each board. Choose between 0 and 10: "))
 for i in range(len(boards)):
-    boards[i] = coloured_grids(boards[i],2)
+    for k in range(n): 
+        plt.figure(figsize=(5,5))
+        # Create colored version of the pattern
+        pattern = coloured_grids1(boards[i])
+                #print(f"Pattern shape: {pattern.shape}") 
+        
+        plt.imshow(pattern)
+        plt.axis('off')
+        
+        filename = f'new_pattern_step_{i//2 + 1}_{"original" if i%2==0 else "inverted"}_{k}.jpg'
+        full_path = os.path.join(save_dir, filename)
+        
+        plt.savefig(full_path, 
+                    dpi=300,
+                    bbox_inches='tight',
+                    pad_inches=0)  
+        plt.close()
 
-
-save_dir = "pattern_images"
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
-
-for i in range(len(boards)):
-    plt.figure(figsize=(5,5))
-    plt.imshow(boards[i], cmap='binary')
-    plt.axis('off')
-    #plt.title(f'Step {i//2 + 1}{"" if i%2==0 else " inverted"}')
-    plt.savefig(f'pattern_step_{i//2 + 1}_{"original" if i%2==0 else "inverted"}.jpg', 
-                dpi=300,
-                bbox_inches='tight',
-                pad_inches=0)  # Removed padding
-    plt.close()
-
-plt.figure(figsize=(5*len(get_steps(size)*2), 5))
-for i in range(len(boards)):
-    plt.subplot(1, len(get_steps(size)*2), i+1)
-
-    plt.imshow(boards[i], cmap='binary')  # using list index
-    plt.title(f'Step {i+1}')
-plt.show()
+print(f"Files saved in: {save_dir}")
